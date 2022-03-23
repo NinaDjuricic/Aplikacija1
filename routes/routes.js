291b367573204889f7fdb72f6e1072e6f9729d86
NinaDjuricic <niky.djuricic2@gmail.com>
2022-03-23 22:29:57 +0100

    Preprvka

--- routes/routes.js
+++ routes/routes.js
@@ -9,22 +9,15 @@ router.post('/signup', signup);
 
 router.get('/private', isAuth);
 
-router.get('/public', async (req, res, next) => {
-    try {
-        res.status(200).json({ message: "here is your public resource" });
-    } catch (err) {
-      res.json({ error: err.message || err.toString() });
-    }
-  });
 
-/*
+
 router.get('/public', (req, res, next) => {
     res.status(200).json({ message: "here is your public resource" });
 });
 
 // will match any other path
-router.use('/', (req, res, next) => {
+router.use('/public', (req, res, next) => {
     res.status(404).json({error : "page not found"});
 });
-*/
+
 export default router;
