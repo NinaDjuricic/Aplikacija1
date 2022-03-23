8a08e5d9abfb55e6d569cb3c1c20978a46dadab1
NinaDjuricic <niky.djuricic2@gmail.com>
2022-03-23 22:31:34 +0100

    Opet

--- routes/routes.js
+++ routes/routes.js
@@ -11,12 +11,12 @@ router.get('/private', isAuth);
 
 
 
-router.get('/public', (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.status(200).json({ message: "here is your public resource" });
 });
 
 // will match any other path
-router.use('/public', (req, res, next) => {
+router.use('/', (req, res, next) => {
     res.status(404).json({error : "page not found"});
 });
 
