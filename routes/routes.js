ea959eb66c999281e4b11bfb49f3a5169f384013
NinaDjuricic <niky.djuricic2@gmail.com>
2022-03-23 21:54:54 +0100

    Opet prepravka

--- routes/routes.js
+++ routes/routes.js
@@ -20,9 +20,6 @@ router.get('/connection-test', (req, res) => {
      });
    });
 
-// will match any other path
-router.use('/', (req, res, next) => {
-    res.status(404).json({error : "page not found"});
-});
+
 
 export default router;
